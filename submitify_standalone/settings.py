d66836615aaa6567d08b04924d707a941e3000f1
Madison Scott-Clary <madison.scott-clary@canonical.com>
2018-05-20 13:18:21 -0700

    Start using django2 and py37

--- submitify_standalone/settings.py
+++ submitify_standalone/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'submitify',
     'usermgmt_standalone',
-    'django_extensions',
+    #'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- usermgmt_standalone/views.py
+++ usermgmt_standalone/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import (
     login,
 )
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db.models import Q
 from django.shortcuts import (
     get_object_or_404,
