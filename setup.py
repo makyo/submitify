6dd99d4c1ab5e4a2a226a99ef5898465cee08aee
Madison Scott-Clary <makyo@drab-makyo.com>
2020-01-15 11:25:30 -0800

    Update deps, bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-submitify',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['prose-wc', 'python-magic'],
@@ -30,11 +30,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
